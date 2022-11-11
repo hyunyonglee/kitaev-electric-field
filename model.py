afbe05dea60ea784a3e0433bbbccf48852697362
hyunyonglee <hyunyong.rhee@gmail.com>
2022-11-12 08:58:59 +0900

    init

---
+++ model.py
@@ -0,0 +1,153 @@
+# Copyright 2022 Hyun-Yong Lee
+
+import numpy as np
+from tenpy.models.lattice import Site, Chain, Square
+from tenpy.models.model import CouplingModel, NearestNeighborModel, MPOModel, CouplingMPOModel
+from tenpy.models import lattice
+from tenpy.tools.params import Config
+from tenpy.networks.site import SpinHalfSite  # if you want to use the predefined site
+import matplotlib.pyplot as plt
+__all__ = ['KITAEV_ELECTRIC_FIELD']
+
+
+class KITAEV_ELECTRIC_FIELD(CouplingModel,MPOModel):
+    
+    def __init__(self, model_params):
+        
+        # 0) read out/set default parameters 
+        if not isinstance(model_params, Config):
+            model_params = Config(model_params, "KITAEV_ELECTRIC_FIELD")
+        Lx = model_params.get('Lx', 1)
+        Ly = model_params.get('Ly', 1)
+        K = model_params.get('K', 1.)
+        hb = model_params.get('hb', 0.)
+        hc = model_params.get('hc', 0.)
+        Eb = model_params.get('Eb', 0.)
+        Ec = model_params.get('Ec', 0.)
+        bc_MPS = model_params.get('bc_MPS', 'infinite')
+        bc = model_params.get('bc', 'periodic')
+        
+        site = SpinHalfSite(conserve=None)
+
+        basis = [ [np.sqrt(3.),0], [0,3] ]
+        pos = [ [0,0], [-np.sqrt(3.)*0.5,0.5], [-np.sqrt(3.)*0.5,1.5], [0,2] ]
+        nn = [ (0, 1, [0,0]), (0, 1, [1,0]), (1, 2, [0,0]), (3, 2, [1,0]), (2, 3, [0,0]), (3, 0, [0,1]) ] 
+        lat = lattice.Lattice( Ls=[Lx, Ly], 
+                              unit_cell=[site, site, site, site], 
+                              basis=basis,
+                              positions=pos, 
+                              bc=bc, 
+                              bc_MPS=bc_MPS)
+        lat.pairs['nearest_neighbors'] = nn
+
+        CouplingModel.__init__(self, lat)
+
+        # Kitaev interaction
+        # x-bond
+        self.add_coupling( -K, 3, 'Sigmax', 2, 'Sigmax', [0, 0])
+        self.add_coupling( -K, 1, 'Sigmax', 0, 'Sigmax', [-1, 0])
+        
+        # y-bond
+        self.add_coupling( -K, 1, 'Sigmay', 0, 'Sigmay', [0, 0])
+        self.add_coupling( -K, 3, 'Sigmay', 2, 'Sigmay', [1, 0])
+        
+        # z-bond
+        self.add_coupling( -K, 1, 'Sigmaz', 2, 'Sigmaz', [0, 0])
+        self.add_coupling( -K, 3, 'Sigmaz', 0, 'Sigmaz', [0, 1])
+
+        
+        # Electric field (b-direction)  A->B: positive
+        # x-bond
+        self.add_coupling( (-1.)*(-Eb)/np.sqrt(2.), 3, 'Sigmay', 2, 'Sigmaz', [0, 0])
+        self.add_coupling( (+1.)*(-Eb)/np.sqrt(2.), 3, 'Sigmaz', 2, 'Sigmay', [0, 0])
+        self.add_coupling( (+1.)*(-Eb)/np.sqrt(2.), 3, 'Sigmaz', 2, 'Sigmax', [0, 0])
+        self.add_coupling( (-1.)*(-Eb)/np.sqrt(2.), 3, 'Sigmax', 2, 'Sigmaz', [0, 0])
+
+        self.add_coupling( (-1.)*(-Eb)/np.sqrt(2.), 1, 'Sigmay', 0, 'Sigmaz', [-1, 0])
+        self.add_coupling( (+1.)*(-Eb)/np.sqrt(2.), 1, 'Sigmaz', 0, 'Sigmay', [-1, 0])
+        self.add_coupling( (+1.)*(-Eb)/np.sqrt(2.), 1, 'Sigmaz', 0, 'Sigmax', [-1, 0])
+        self.add_coupling( (-1.)*(-Eb)/np.sqrt(2.), 1, 'Sigmax', 0, 'Sigmaz', [-1, 0])
+        
+        # y-bond
+        self.add_coupling( (-1.)*(-Eb)/np.sqrt(2.), 1, 'Sigmay', 0, 'Sigmaz', [0, 0])
+        self.add_coupling( (+1.)*(-Eb)/np.sqrt(2.), 1, 'Sigmaz', 0, 'Sigmay', [0, 0])
+        self.add_coupling( (+1.)*(-Eb)/np.sqrt(2.), 1, 'Sigmaz', 0, 'Sigmax', [0, 0])
+        self.add_coupling( (-1.)*(-Eb)/np.sqrt(2.), 1, 'Sigmax', 0, 'Sigmaz', [0, 0])
+
+        self.add_coupling( (-1.)*(-Eb)/np.sqrt(2.), 3, 'Sigmay', 2, 'Sigmaz', [1, 0])
+        self.add_coupling( (+1.)*(-Eb)/np.sqrt(2.), 3, 'Sigmaz', 2, 'Sigmay', [1, 0])
+        self.add_coupling( (+1.)*(-Eb)/np.sqrt(2.), 3, 'Sigmaz', 2, 'Sigmax', [1, 0])
+        self.add_coupling( (-1.)*(-Eb)/np.sqrt(2.), 3, 'Sigmax', 2, 'Sigmaz', [1, 0])
+        
+        # z-bond
+        self.add_coupling( (-1.)*(-Eb)/np.sqrt(2.), 1, 'Sigmay', 2, 'Sigmaz', [0, 0])
+        self.add_coupling( (+1.)*(-Eb)/np.sqrt(2.), 1, 'Sigmaz', 2, 'Sigmay', [0, 0])
+        self.add_coupling( (+1.)*(-Eb)/np.sqrt(2.), 1, 'Sigmaz', 2, 'Sigmax', [0, 0])
+        self.add_coupling( (-1.)*(-Eb)/np.sqrt(2.), 1, 'Sigmax', 2, 'Sigmaz', [0, 0])
+
+        self.add_coupling( (-1.)*(-Eb)/np.sqrt(2.), 3, 'Sigmay', 0, 'Sigmaz', [0, 1])
+        self.add_coupling( (+1.)*(-Eb)/np.sqrt(2.), 3, 'Sigmaz', 0, 'Sigmay', [0, 1])
+        self.add_coupling( (+1.)*(-Eb)/np.sqrt(2.), 3, 'Sigmaz', 0, 'Sigmax', [0, 1])
+        self.add_coupling( (-1.)*(-Eb)/np.sqrt(2.), 3, 'Sigmax', 0, 'Sigmaz', [0, 1])
+
+
+        # Electric field (c-direction)  A->B: positive
+        # x-bond
+        self.add_coupling( (+1.)*(-Ec)/np.sqrt(3.), 3, 'Sigmax', 2, 'Sigmay', [0, 0])
+        self.add_coupling( (-1.)*(-Ec)/np.sqrt(3.), 3, 'Sigmay', 2, 'Sigmax', [0, 0])
+        self.add_coupling( (+1.)*(-Ec)/np.sqrt(3.), 3, 'Sigmay', 2, 'Sigmaz', [0, 0])
+        self.add_coupling( (-1.)*(-Ec)/np.sqrt(3.), 3, 'Sigmaz', 2, 'Sigmay', [0, 0])
+        self.add_coupling( (+1.)*(-Ec)/np.sqrt(3.), 3, 'Sigmaz', 2, 'Sigmax', [0, 0])
+        self.add_coupling( (-1.)*(-Ec)/np.sqrt(3.), 3, 'Sigmax', 2, 'Sigmaz', [0, 0])
+
+        self.add_coupling( (+1.)*(-Ec)/np.sqrt(3.), 1, 'Sigmax', 0, 'Sigmay', [-1, 0])
+        self.add_coupling( (-1.)*(-Ec)/np.sqrt(3.), 1, 'Sigmay', 0, 'Sigmax', [-1, 0])
+        self.add_coupling( (+1.)*(-Ec)/np.sqrt(3.), 1, 'Sigmay', 0, 'Sigmaz', [-1, 0])
+        self.add_coupling( (-1.)*(-Ec)/np.sqrt(3.), 1, 'Sigmaz', 0, 'Sigmay', [-1, 0])
+        self.add_coupling( (+1.)*(-Ec)/np.sqrt(3.), 1, 'Sigmaz', 0, 'Sigmax', [-1, 0])
+        self.add_coupling( (-1.)*(-Ec)/np.sqrt(3.), 1, 'Sigmax', 0, 'Sigmaz', [-1, 0])
+        
+        # y-bond
+        self.add_coupling( (+1.)*(-Ec)/np.sqrt(3.), 1, 'Sigmax', 0, 'Sigmay', [0, 0])
+        self.add_coupling( (-1.)*(-Ec)/np.sqrt(3.), 1, 'Sigmay', 0, 'Sigmax', [0, 0])
+        self.add_coupling( (+1.)*(-Ec)/np.sqrt(3.), 1, 'Sigmay', 0, 'Sigmaz', [0, 0])
+        self.add_coupling( (-1.)*(-Ec)/np.sqrt(3.), 1, 'Sigmaz', 0, 'Sigmay', [0, 0])
+        self.add_coupling( (+1.)*(-Ec)/np.sqrt(3.), 1, 'Sigmaz', 0, 'Sigmax', [0, 0])
+        self.add_coupling( (-1.)*(-Ec)/np.sqrt(3.), 1, 'Sigmax', 0, 'Sigmaz', [0, 0])
+        
+        self.add_coupling( (+1.)*(-Ec)/np.sqrt(3.), 3, 'Sigmax', 2, 'Sigmay', [1, 0])
+        self.add_coupling( (-1.)*(-Ec)/np.sqrt(3.), 3, 'Sigmay', 2, 'Sigmax', [1, 0])
+        self.add_coupling( (+1.)*(-Ec)/np.sqrt(3.), 3, 'Sigmay', 2, 'Sigmaz', [1, 0])
+        self.add_coupling( (-1.)*(-Ec)/np.sqrt(3.), 3, 'Sigmaz', 2, 'Sigmay', [1, 0])
+        self.add_coupling( (+1.)*(-Ec)/np.sqrt(3.), 3, 'Sigmaz', 2, 'Sigmax', [1, 0])
+        self.add_coupling( (-1.)*(-Ec)/np.sqrt(3.), 3, 'Sigmax', 2, 'Sigmaz', [1, 0])
+        
+        # z-bond
+        self.add_coupling( (+1.)*(-Ec)/np.sqrt(3.), 1, 'Sigmax', 2, 'Sigmay', [0, 0])
+        self.add_coupling( (-1.)*(-Ec)/np.sqrt(3.), 1, 'Sigmay', 2, 'Sigmax', [0, 0])
+        self.add_coupling( (+1.)*(-Ec)/np.sqrt(3.), 1, 'Sigmay', 2, 'Sigmaz', [0, 0])
+        self.add_coupling( (-1.)*(-Ec)/np.sqrt(3.), 1, 'Sigmaz', 2, 'Sigmay', [0, 0])
+        self.add_coupling( (+1.)*(-Ec)/np.sqrt(3.), 1, 'Sigmaz', 2, 'Sigmax', [0, 0])
+        self.add_coupling( (-1.)*(-Ec)/np.sqrt(3.), 1, 'Sigmax', 2, 'Sigmaz', [0, 0])
+        
+        self.add_coupling( (+1.)*(-Ec)/np.sqrt(3.), 3, 'Sigmax', 0, 'Sigmay', [0, 1])
+        self.add_coupling( (-1.)*(-Ec)/np.sqrt(3.), 3, 'Sigmay', 0, 'Sigmax', [0, 1])
+        self.add_coupling( (+1.)*(-Ec)/np.sqrt(3.), 3, 'Sigmay', 0, 'Sigmaz', [0, 1])
+        self.add_coupling( (-1.)*(-Ec)/np.sqrt(3.), 3, 'Sigmaz', 0, 'Sigmay', [0, 1])
+        self.add_coupling( (+1.)*(-Ec)/np.sqrt(3.), 3, 'Sigmaz', 0, 'Sigmax', [0, 1])
+        self.add_coupling( (-1.)*(-Ec)/np.sqrt(3.), 3, 'Sigmax', 0, 'Sigmaz', [0, 1])
+
+        # on-site
+        for u in range(len(self.lat.unit_cell)):
+            self.add_onsite( +hb/np.sqrt(2.), u, 'Sigmax')
+            self.add_onsite( -hb/np.sqrt(2.), u, 'Sigmay')
+            
+            self.add_onsite( -hc/np.sqrt(3.), u, 'Sigmax')
+            self.add_onsite( -hc/np.sqrt(3.), u, 'Sigmay')
+            self.add_onsite( -hc/np.sqrt(3.), u, 'Sigmaz')
+        
+        
+        
+        MPOModel.__init__(self, lat, self.calc_H_MPO())
+
+
---
+++ run_model.py
@@ -0,0 +1,140 @@
+# Copyright 2022 Hyun-Yong Lee
+
+import numpy as np
+import model
+from tenpy.networks.mps import MPS
+from tenpy.algorithms import dmrg
+from tenpy.algorithms import tebd
+from tenpy.tools.process import mkl_set_nthreads
+import os
+import os.path
+import argparse, sys
+import pickle
+
+def ensure_dir(f):
+    d=os.path.dirname(f)
+    if not os.path.exists(d):
+        os.makedirs(d)
+    return d
+
+
+mkl_set_nthreads(64)
+
+import logging.config
+conf = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {'custom': {'format': '%(levelname)-8s: %(message)s'}},
+    'handlers': {'to_file': {'class': 'logging.FileHandler',
+                             'filename': 'log',
+                             'formatter': 'custom',
+                             'level': 'INFO',
+                             'mode': 'a'},
+                'to_stdout': {'class': 'logging.StreamHandler',
+                              'formatter': 'custom',
+                              'level': 'INFO',
+                              'stream': 'ext://sys.stdout'}},
+    'root': {'handlers': ['to_stdout', 'to_file'], 'level': 'DEBUG'},
+}
+logging.config.dictConfig(conf)
+
+parser=argparse.ArgumentParser()
+parser.add_argument("--Lx", help="Length of Cylinder")
+parser.add_argument("--Ly", help="Circumference of Cylinder")
+parser.add_argument("--K", help="Kitaev interaction")
+parser.add_argument("--hb", help="Magnetic field along b-direction")
+parser.add_argument("--hc", help="Magnetic field along c-direction")
+parser.add_argument("--Eb", help="Electric field along b-direction")
+parser.add_argument("--Ec", help="Electric field along c-direction")
+parser.add_argument("--CHI", help="Bond dimension")
+parser.add_argument("--RM", help="'random': randomize initial state")
+parser.add_argument("--TOL", help="Convergence criteria for Entanglent Entropy")
+parser.add_argument("--EXC", help="'ON': calculate the 1st excited state")
+args=parser.parse_args()
+
+#python run_model.py --Lx=1 --Ly=2 --CHI=10 --hb=0.01 --K=1.0 --RM=None --TOL=1.0e-6 --EXC=None
+
+Lx = int(args.Lx)
+Ly = int(args.Ly)
+K = float(args.K)
+hb = float(args.hb)
+hc = float(args.hc)
+Eb = float(args.Eb)
+Ec = float(args.Ec)
+CHI = int(args.CHI)
+RM = args.RM
+TOL = float(args.TOL)
+EXC = args.EXC
+
+model_params = {
+    "Lx": Lx,
+    "Ly": Ly,
+    "K": K,
+    "hb": hb,
+    "hc": hc,
+    "Eb": Eb,
+    "Ec": Ec
+}
+
+print("\n\n\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+
+M = model.KITAEV_ELECTRIC_FIELD(model_params)
+product_state = ["up"] * M.lat.N_sites
+
+psi = MPS.from_product_state(M.lat.mps_sites(), product_state, bc=M.lat.bc_MPS)
+if RM == 'random':
+    TEBD_params = {'N_steps': 4, 'trunc_params':{'chi_max': 4}, 'verbose': 0}
+    eng = tebd.RandomUnitaryEvolution(psi, TEBD_params)
+    eng.run()
+    psi.canonical_form() 
+
+dchi = int(CHI/5)
+chi_list = {0: 32, 5: 64, 10: CHI}
+
+dmrg_params = {
+    # 'mixer': True,  # setting this to True helps to escape local minima
+    'mixer' : dmrg.SubspaceExpansion,
+    'mixer_params': {
+        'amplitude': 1.e-3,
+        'decay': 2.0,
+        'disable_after': 30
+    },
+    'trunc_params': {
+        'chi_max': CHI,
+        'svd_min': 1.e-9
+    },
+    # 'lanczos_params': {
+    #         'N_min': 5,
+    #         'N_max': 20
+    # },
+    'chi_list': chi_list,
+    'max_E_err': 1.0e-8,
+    'max_S_err': TOL,
+    'max_sweeps': 100,
+    'combine' : True
+}
+
+'''
+ensure_dir("observables/")
+ensure_dir("entanglement/")
+ensure_dir("logs/")
+ensure_dir("mps/")
+'''
+
+# ground state
+eng = dmrg.TwoSiteDMRGEngine(psi, M, dmrg_params)
+E, psi = eng.run()  # equivalent to dmrg.run() up to the return parameters.
+psi.canonical_form() 
+
+Mz = psi.expectation_value("Sigmaz")
+EE = psi.entanglement_entropy()
+ES = psi.entanglement_spectrum()
+
+
+flux = psi.expectation_value_term([('Sigmax',1),('Sigmay',2),('Sigmaz',0),('Sigmax',10),('Sigmay',9),('Sigmaz',3)])
+
+print(Mz)
+print(flux)
+
+
+print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n\n\n")
