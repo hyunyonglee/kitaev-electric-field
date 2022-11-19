25cbe4738b2f4f0417140fb45b90652ad49d1ea8
hyunyonglee <hyunyong.rhee@gmail.com>
2022-11-19 19:41:29 +0900

    revise

--- run_model.py
+++ run_model.py
@@ -203,8 +203,8 @@ if __name__=='__main__':
         'mixer' : dmrg.SubspaceExpansion,
         'mixer_params': {
             'amplitude': 1.e-2,
-            'decay': 2.0,
-            'disable_after': 30
+            'decay': 1.5,
+            'disable_after': 50
         },
         'trunc_params': {
             'chi_max': chi,
@@ -217,7 +217,7 @@ if __name__=='__main__':
         'chi_list': chi_list,
         'max_E_err': 1.0e-8,
         'max_S_err': tol,
-        'max_sweeps': 500,
+        'max_sweeps': 1000,
         'combine' : True
     }
 
