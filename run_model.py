eacee825b92eb5b69d49065703a5cb6ee4f44538
hyunyonglee <hyunyong.rhee@gmail.com>
2022-11-19 21:21:56 +0900

    revise

--- run_model.py
+++ run_model.py
@@ -208,8 +208,8 @@ if __name__=='__main__':
         chi_list[5*i] = (i+1)*50
     
     dmrg_params = {
-        'mixer': True,  # setting this to True helps to escape local minima
-        # 'mixer' : dmrg.SubspaceExpansion,
+        # 'mixer': True,  # setting this to True helps to escape local minima
+        'mixer' : dmrg.SubspaceExpansion,
         'mixer_params': {
             'amplitude': 1.e-3,
             'decay': 2.0,
