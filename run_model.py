3459d75054181ca298e272c1875a95d2d89ca669
hyunyonglee <hyunyong.rhee@gmail.com>
2022-11-19 21:27:39 +0900

    revise

--- run_model.py
+++ run_model.py
@@ -209,7 +209,7 @@ if __name__=='__main__':
     
     dmrg_params = {
         # 'mixer': True,  # setting this to True helps to escape local minima
-        'mixer' : dmrg.SubspaceExpansion,
+        'mixer' : dmrg.DensityMatrixMixer, # dmrg.SubspaceExpansion
         'mixer_params': {
             'amplitude': 1.e-3,
             'decay': 2.0,
